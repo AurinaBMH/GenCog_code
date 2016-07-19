35ca0e991f14b3b4ec6aef406accd993e60f38e3
AurinaBMH <aurina.arnatkeviciute@monash.edu>
2016-07-19 18:18:30 +1000

    Additionl folder deleted

--- Connectome/Connectomes/LengthTests.m
+++
@@ -1,9 +0,0 @@
-for i=1:15 L(i,:,:) = len{i}; end
-
-Le = squeeze(sum(L,1) ./ sum(L(:,:,:)~=0));
-A = logical(Adj);
-Le = squeeze(Le);
-B = A.*Le;
-figure; imagesc(B);
-Bl = B(:);
-figure; histogram(nonzeros(Bl), 20);
