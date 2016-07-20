71203c0a9d4dba05e29411ffa4ce824ee1ce45da
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2016-07-20 12:25:33 +1000

    Script updated to include a plot for average coexpression depending on weight threshold for both connected and unconnected links.

--- Microarray/ConsistencyWeightCheck.m
+++ Microarray/ConsistencyWeightCheck.m
@@ -2,14 +2,14 @@
 % forming a group matrix and weights. Purpose - to set up a threshold for
 % binarising the matrix. Coexpression between distance Uncorrected ROIs
 % should be higher for connected versus Connected nodes.
-%clear all; close all;
+clear all; close all;
 
 cd('/Users/Aurina/GoogleDrive/Genetics_connectome/Gen_Cog/Data/Microarray/');
 load ('FACT_cust100_HCP_good.mat');
 
 
 DSs = [2 5 10 50 100];
-Thresholds = [0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1];
+Thresholds = [0.6];
 NumWeights = 1000;
 weightMeasure = {'density'};
 WEIGHT = density; 
@@ -19,10 +19,12 @@ for l=1:length(DSs)
     
     DS = DSs(l);
     cd('/Users/Aurina/GoogleDrive/Genetics_connectome/Gen_Cog/Data/Microarray/')
-    load(sprintf('%dDSSpearmanExpressionCust100CorrectedSigmoid.mat', DS));
+    load(sprintf('%dDSExpressionCust100Uncorrected.mat', DS));
 
     Tvals = zeros(length(Thresholds), NumWeights);
     Pvals = zeros(length(Thresholds), NumWeights);
+    ConUncon = zeros(NumWeights, 2);
+    ConUnconSD = zeros(NumWeights, 2, length(DSs));
     
     for i=1:length(Thresholds)
         
@@ -39,12 +41,27 @@ for l=1:length(DSs)
                 
                 w = weights(j);
                 mask = log(Adj)>w;
-                [y,e, Con, Uncon, t, p] = CoexpConnectVSUnconnect(Exp,mask);
+                [y,e, dataCell,t, p] = CoexpConnectVSUnconnect(Exp,mask);
                 Tvals(i,j) = t;
                 Pvals(i,j) = p;
+                ConUncon(j,:,l) = y;
+                ConUnconSD(j,:,l) = e;
                 
                 
             end
+            
+            x1 = weights';
+            y1 = ConUncon(:,1,l); y2 = ConUncon(:,2,l);
+            dy1 = ConUnconSD(:,1,l); dy2 = ConUnconSD(:,2,l); 
+
+            FigHandle = figure;
+            set(FigHandle, 'Position', [100, 100, 1049, 895]);
+
+            subplot(4,1,[1 2]);shadedErrorBar(x1,y2,dy2, 'b', 1); hold on; shadedErrorBar(x1,y1,dy1, 'r',1);
+            axis([-6 1.4 -0.2 0.4]);
+            title (sprintf('GrdThreshold %dperc\n %dSD Average coexpression dependency on weight threshold', grpThr*100, DS)); xlabel('log(weight)'); ylabel('Average coexpression value');
+            subplot(4,1,3); histogram(nonzeros(log(Adj(:))), 50); title('Weight distribution of the original matrix');
+            xlabel('log(weight)'); ylabel('number of links'); 
             
         elseif strcmp(weightMeasure{1}, 'FA')
             
@@ -68,9 +85,9 @@ for l=1:length(DSs)
         
     end
     
-    figure; imagesc(Tvals); colormap([flipud(BF_getcmap('blues',20));1,1,1;BF_getcmap('reds',20)]);
-    caxis([-7 7]);
-    title(sprintf('%dDS Corrected T values', DS)); xlabel('Weight threshold'); ylabel('Consistency between subjects, %'); colorbar;
+    subplot(4,1,4); imagesc(Tvals); colormap([flipud(BF_getcmap('blues',20));1,1,1;BF_getcmap('reds',20)]);
+    caxis([0 30]);colorbar('southoutside');
+    title(sprintf('%dDS Corrected T values', DS)); xlabel('Weight threshold'); ylabel('Consistency between subjects, %');
     
     
 end
