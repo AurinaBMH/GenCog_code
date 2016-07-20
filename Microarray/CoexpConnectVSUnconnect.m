07e071b74264a29314ad512a80f6be7e079daf22
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2016-07-20 12:24:30 +1000

    Function output changed  to include dataCell where distributions of connected and unconnected link coexpression values are saved.

--- Microarray/CoexpConnectVSUnconnect.m
+++ Microarray/CoexpConnectVSUnconnect.m
@@ -1,8 +1,17 @@
-function [y,e, Con, Uncon, t, p] = CoexpConnectVSUnconnect(Exp,Adj)
+function [y,e, dataCell,t, p] = CoexpConnectVSUnconnect(Exp,Adj)
 
 % connected vs unconnected
 % Rexp - coexpression matrix
 % Adj - adjecency matrix
+
+%% output
+% y - mean values for connected and unconnected links
+% e - SD for connected and unconnected links
+% dataCell{1,1} - distribution of connected values
+% dataCell{2,1} - - distribution of unconnected values
+% t - ttest2 t value
+% p - ttest2 p value
+
 
 mask = logical(Adj);
 
@@ -21,7 +30,7 @@ mask = logical(Adj);
        dataCell = cell(2,1);
        dataCell{1,1} = Con; dataCell{2,1} = Uncon; 
        
-JitteredParallelScatter(dataCell)
+%JitteredParallelScatter(dataCell)
 %ylabel('Average coexpression'); legend('Connecteced', 'Unconnected');
 [h,p,ci,stats] = ttest2(Con,Uncon, 'Vartype','unequal');
 t = stats.tstat;
