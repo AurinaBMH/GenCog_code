1434f8ce8eb448fae2ba8b86ca55cf2ba9a9f105
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2016-07-25 18:39:24 +1000

    Plot only consistency - weight matrix instead all average curves with subplots.

--- Microarray/ConsistencyWeightCheck.m
+++ Microarray/ConsistencyWeightCheck.m
@@ -5,11 +5,11 @@
 clear all; close all;
 
 cd('/Users/Aurina/GoogleDrive/Genetics_connectome/Gen_Cog/Data/Microarray/');
-load ('FACT_cust100_HCP_good.mat');
+load ('FACT_custom200ANDaseg_withdenoise.mat');
 
 
 DSs = [2 5 10 50 100];
-Thresholds = [0.6];
+Thresholds = [0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1];
 NumWeights = 1000;
 weightMeasure = {'density'};
 WEIGHT = density; 
@@ -19,7 +19,7 @@ for l=1:length(DSs)
     
     DS = DSs(l);
     cd('/Users/Aurina/GoogleDrive/Genetics_connectome/Gen_Cog/Data/Microarray/')
-    load(sprintf('%dDSExpressionCust100Uncorrected.mat', DS));
+    load(sprintf('%dDSSpearmanExpressionCust100CorrectedSigmoid.mat', DS));
 
     Tvals = zeros(length(Thresholds), NumWeights);
     Pvals = zeros(length(Thresholds), NumWeights);
@@ -31,7 +31,7 @@ for l=1:length(DSs)
         grpThr = Thresholds(i);
         adjGrp = connectomeGroupThreshold(WEIGHT, grpThr, 2);
         Adj = adjGrp(1:100, 1:100);
-        
+        Adj(isnan(Adj))=0;
         if strcmp(weightMeasure{1}, 'density') || strcmp(weightMeasure{1}, 'count')
             
             wmin = min(log(nonzeros(Adj(:)))); wmax = max(log(Adj(:)));
@@ -49,20 +49,20 @@ for l=1:length(DSs)
                 
                 
             end
-            
-            x1 = weights';
-            y1 = ConUncon(:,1,l); y2 = ConUncon(:,2,l);
-            dy1 = ConUnconSD(:,1,l); dy2 = ConUnconSD(:,2,l); 
+           
+%             x1 = weights';
+%             y1 = ConUncon(:,1,l); y2 = ConUncon(:,2,l);
+%             dy1 = ConUnconSD(:,1,l); dy2 = ConUnconSD(:,2,l); 
 
-            FigHandle = figure;
-            set(FigHandle, 'Position', [100, 100, 1049, 895]);
-
-            subplot(4,1,[1 2]);shadedErrorBar(x1,y2,dy2, 'b', 1); hold on; shadedErrorBar(x1,y1,dy1, 'r',1);
-            axis([-6 1.4 -0.2 0.4]);
-            title (sprintf('GrdThreshold %dperc\n %dSD Average coexpression dependency on weight threshold', grpThr*100, DS)); xlabel('log(weight)'); ylabel('Average coexpression value');
-            subplot(4,1,3); histogram(nonzeros(log(Adj(:))), 50); title('Weight distribution of the original matrix');
-            xlabel('log(weight)'); ylabel('number of links'); 
-            
+%             FigHandle = figure;
+%             set(FigHandle, 'Position', [100, 100, 1049, 895]);
+% 
+%             subplot(4,1,[1 2]);shadedErrorBar(x1,y2,dy2, 'b', 1); hold on; shadedErrorBar(x1,y1,dy1, 'r',1);
+%             axis([-6 1.4 -0.2 0.4]);
+%             title (sprintf('GrdThreshold %dperc\n %dSD Average coexpression dependency on weight threshold', grpThr*100, DS)); xlabel('log(weight)'); ylabel('Average coexpression value');
+%             subplot(4,1,3); histogram(nonzeros(log(Adj(:))), 50); title('Weight distribution of the original matrix');
+%             xlabel('log(weight)'); ylabel('number of links'); 
+%             
         elseif strcmp(weightMeasure{1}, 'FA')
             
             wmin = min(nonzeros(Adj(:))); wmax = max(Adj(:));
@@ -85,8 +85,9 @@ for l=1:length(DSs)
         
     end
     
-    subplot(4,1,4); imagesc(Tvals); colormap([flipud(BF_getcmap('blues',20));1,1,1;BF_getcmap('reds',20)]);
-    caxis([0 30]);colorbar('southoutside');
+    %subplot(4,1,4); 
+    figure; imagesc(Tvals); colormap([flipud(BF_getcmap('blues',20));1,1,1;BF_getcmap('reds',20)]);
+    caxis([0 12]);colorbar; %colorbar('southoutside');
     title(sprintf('%dDS Corrected T values', DS)); xlabel('Weight threshold'); ylabel('Consistency between subjects, %');
     
     
